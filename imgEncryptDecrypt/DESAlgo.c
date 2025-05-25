8df7f629667964561fdd5b0aea1cfe9783349567
GitWorkingTime <113561439+GitWorkingTime@users.noreply.github.com>
2025-05-25 14:12:13 -0600

    copied binary converter online
    https://www.w3resource.com/c-programming-exercises/c-snippets/print-binary-format-in-c-using-printf-alternatives.php (source)

--- imgEncryptDecrypt/DESAlgo.c
+++ imgEncryptDecrypt/DESAlgo.c
@@ -3,9 +3,18 @@
 long M = 0x0123456789ABCDEF;
 long K = 0x133457799BBCDFF1;
 
+void printBinaryWithPadding(long num) {
+    for (int i = sizeof(long) * 8 - 1; i >= 0; i--) {
+        printf("%ld", (num >> i) & 1);
+        if (i % 8 == 0) printf(" "); // Group by 4 bits for readability
+    }
+    printf("\n");
+}
+
 int main(){
-	printf("M = %ld\n", M);
-	printf("K = %ld\n", K);
+	printf("M: \n");
+	printBinaryWithPadding(M);
+	printf("\n");
 
 	return 0;
 }
